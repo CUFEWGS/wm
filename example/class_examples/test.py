97eab1fbb8404a561abb377a4329b9043b1f8f37
CUFEWGS <wanggs94@outlook.com>
2019-05-28 23:53:58 +0800

    add some examples in eg_class

--- example/class_examples/test.py
+++ example/class_examples/test.py
@@ -125,3 +125,36 @@ from example.class_examples.setwrapper import Set
 x = Set([1, 3, 5, 7])
 print(x.union(Set([1, 4, 7])))
 print(x | Set([1, 4, 6]))
+
+class C:
+    # __slots__ = ['B']
+    pass
+
+class D(C):
+    __slots__ = ['a']
+
+C.__dict__.keys()
+D.__dict__.keys()
+
+X = D()
+X.a = 1
+X.b = 1
+
+class operators:
+    def __getattr__(self, name): # On undefined reference
+        if name == 'age':
+            return 40
+        else:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value): # On all assignments
+        print('set: %s %s' % (name, value))
+        if name == 'age':
+            self.__dict__['_age'] = value # Or object.__setattr__()
+        else:
+            self.__dict__[name] = value
+
+x = operators()
+x.age
+x.age = 41
+x._age
